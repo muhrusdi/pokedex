f74c3285f8c577f2d29a62d44b49623f8450ebd0
Muhammad Rusdi <muhammadrusdiid@gmail.com>
2020-02-23 20:26:19 +0700

    fix other bugs

--- src/containers/bookmark/index.tsx
+++ src/containers/bookmark/index.tsx
@@ -59,7 +59,7 @@ const Bookmark: React.FC<Props> = props => {
               </li>
             ))}
           </ListStyled>
-          {!Object.keys(bookmarks).length && (
+          {!Object.keys(bookmarks || {}).length && (
             <div>
               <Empty />
             </div>
